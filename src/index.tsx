d581f6689ffbc8a65b6f18ed9f8e13cff43c7930
xomastyle- <homenich101@gmail.com>
2022-11-08 15:17:35 +0200

    provide default rtk

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { setupStore } from './redux';
+import { Provider } from 'react-redux';
+
+
+const store = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
